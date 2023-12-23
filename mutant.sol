8736df8f51dc63cf27fd759590b8409621104315
taptap-kor <tyy8282@gmail.com>
2023-12-23 23:16:11 +0900

    build: second

--- mutant.sol
+++ mutant.sol
@@ -6,9 +6,10 @@ pragma solidity ^0.8.20;
 // Voting Governance
 // Can upgrade your NFT with points
 // Get Point from Activation Community and Staking NFT
+// If you upgrade your NFT, then you can access more authorizy
+
 
 // Key NFT
 // ERC-1155
 // Access to DMRS by Doctor Role 
 // If doctor confirmed, then airdrop NFT to doctor's address
-//
