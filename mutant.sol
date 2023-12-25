72f139ae82e2d6c120c09c174e286e640484f6a0
taptap-kor <tyy8282@gmail.com>
2023-12-25 23:34:15 +0900

    feat: readmd

--- mutant.sol
+++ mutant.sol
@@ -1,18 +1,2 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-// Mutant NFT
-// ERC-721
-// Voting Governance
-// Can upgrade your NFT with points
-// Get Point from Activation Community and Staking NFT
-// If you upgrade your NFT, then you can access more authorizy
-
-
-// Key NFT
-// ERC-1155
-// Access to DMRS by Doctor Role 
-// If doctor confirmed, then airdrop NFT to doctor's address
-
-
-// Three Contract
