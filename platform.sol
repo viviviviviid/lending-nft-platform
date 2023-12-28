18b3e97fa818daaa4003c7b26e2b7daf678a0e6e
taptap-kor <tyy8282@gmail.com>
2023-12-28 23:52:57 +0900

    build: building platform

--- platform.sol
+++ platform.sol
@@ -4,3 +4,19 @@ pragma solidity ^0.8.20;
 import './libraries/SafeMath.sol';
 import './lending.sol';
 import './borrow.sol';
+
+contract Lending {
+     struct NFT{
+        string nftHash;
+        uint256 amount;
+        uint256 duration;
+        uint256 APR;
+    }
+
+    mapping (address => NFT) public NFTmapping;
+
+    function listing(string memory nftHash, uint256 amount, uint256 duration, uint256 APR) public {
+        NFT memory newNFT = NFT(nftHash, amount, duration, APR);
+        NFTmapping[msg.sender] = newNFT;
+    }
+}
