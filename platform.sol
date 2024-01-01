6bcd500e6e0845c04bb197982f76bd2e7118db70
taptap-kor <tyy8282@gmail.com>
2024-01-01 22:03:27 +0900

    feat: listing

--- platform.sol
+++ platform.sol
@@ -1,24 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import './libraries/SafeMath.sol';
-import './lending.sol';
-import './borrow.sol';
-
 contract Lending {
+
+    event NewListing(address owner, NFT info);
+
      struct NFT{
+        uint256 idx;
         string nftHash;
         uint256 amount;
         uint256 duration;
         uint256 APR;
     }
 
-    mapping (address => NFT) public NFTmapping;
+   uint256 idx = 0;
 
-    function listing(string memory nftHash, uint256 amount, uint256 duration, uint256 APR) public {
-        NFT memory newNFT = NFT(nftHash, amount, duration, APR);
-        NFTmapping[msg.sender] = newNFT;
+    mapping (address => NFT) public list;
 
-        
+    function _listing(string memory nftHash, uint256 amount, uint256 duration, uint256 APR) public {
+        NFT memory info = NFT(idx, nftHash, amount, duration, APR);
+        list[msg.sender] = info;
+        emit NewListing(msg.sender, info);
+        idx++;
+    }
+
+    // function _delisting
+
+    function _viewListing() public view returns (NFT memory){
+        return list[msg.sender];
     }
 }
