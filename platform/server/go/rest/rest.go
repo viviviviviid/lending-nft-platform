980af89f72ce491c1ce3e4a1fae05feba8b8c241
taptap-kor <tyy8282@gmail.com>
2024-01-06 03:36:45 +0900

    build: functions

--- platform/server/go/rest/rest.go
+++ platform/server/go/rest/rest.go
@@ -12,7 +12,22 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func listing(rw http.ResponseWriter, r *http.Request) {
+}
+
+func delisting(rw http.ResponseWriter, r *http.Request) {
+}
+
+func buy(rw http.ResponseWriter, r *http.Request) {
+}
+
 func Start() {
 	http.HandleFunc("/", documentation)
 	http.ListenAndServe(":8080", nil)
 }
+
+// API
+// 전부 트잭이 들어간 뒤에, 컨펌 응답이 오면 DB CURD.
+// listing
+// delisting
+// buy -> delisting
